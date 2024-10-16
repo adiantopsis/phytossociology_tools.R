1d36aed18896f8676328001162372950a14cb94c
Denilson da Silva Machado <00316621@ufrgs.br>
2024-10-16 17:49:55 -0300

    Update example_script.R

--- R/example_script.R
+++ R/example_script.R
@@ -1,5 +1,13 @@
+## Loading functions
 source("func/FitoR.txt")
+source("func/FitoR_camp.txt")
+
+#### Forest
 data<-read.csv(file="data/example_tab.csv", sep=";", dec=",")
-data_corrected<-dap.eq(data)
+data_corrected<-dap.eq(data) # Using only equivalent diameter 
 fitoR(x=data, area=100, VI="cottam", filename="my_result")
 fitoR(x=data_corrected, area=100, VI="cottam", filename="my_result")
+
+### Grassland
+data_camp <- read.csv(file="data/example_tab_camp.csv", sep=";", dec=",")
+fitoR_camp(x=data_camp, area=1, filename="my_result_camp")
